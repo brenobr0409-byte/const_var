25f26bf525b5662fede86839eb461c2e57ddba5e
Breno0409-Byte <brenobr0409@gmail.com>
2026-02-20 17:08:34 -0300

    Create var_cons.php

---
+++ var_cons.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cons_var</title>
+</head>
+<body>
+    <?php 
+    
+    //int = numero;
+    //string = caractere;
+    //boleano = true/false
+    //float = 0.0
+    
+    //constante se inicia com a palavra "define"
+    /*entre aspas duplas
+    1º constante
+    2º valor*/
+    
+    define("instituicao", "centro de educação tec. do amazonas");
+
+    $aluno = "Breno";
+    $idade = "130";
+
+    echo "Olá aluno, $aluno, idade, $idade, bem vindo ao " . instituicao
+
+    ?>
+</body>
+</html>
